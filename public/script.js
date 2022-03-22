0a17355c506d04418dcd547f4da126e1f4a4b5ed
qwe1234k <bbyl0124@gmail.com>
2022-03-22 15:31:09 +0900

    korea Time upgrade ver

--- public/script.js
+++ public/script.js
@@ -1,8 +1,16 @@
-const CryptoJS = require("crypto-js")
+// const CryptoJS = require("crypto-js")
 
 
-var today = new Date();
-var NowDate = today.toLocaleString()
+// var today = new Date();
+// var NowDate = today.toLocaleString()
 
-var hash = CryptoJS.SHA256(NowDate);
-console.log(hash['words'][0])
+// var hash = CryptoJS.SHA256(NowDate);
+// console.log(hash['words'][0])
+
+
+
+const moment = require('moment'); 
+require('moment-timezone'); 
+moment.tz.setDefault("Asia/Seoul"); 
+const date = moment().format('YYYY-MM-DD HH:mm:ss'); 
+console.log(date);
