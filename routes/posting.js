c153805a478d89d72e5a39125d5bc95345008f37
qwe1234k <bbyl0124@gmail.com>
2022-03-22 15:14:08 +0900

    korea time

--- routes/posting.js
+++ routes/posting.js
@@ -16,8 +16,12 @@ router.post("/posting", async (req, res) => {
 
     const {Name, Pw, Title, Content} = req.body
 
-    const today = new Date();
-    const NowDate = today.toLocaleString()
+    const now = new Date(); // 현재 시간
+    const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000); // 현재 시간을 utc로 변환한 밀리세컨드값
+    const koreaTimeDiff = 9 * 60 * 60 * 1000; // 한국 시간은 UTC보다 9시간 빠름(9시간의 밀리세컨드 표현)
+    const NowDate = new Date(utcNow + koreaTimeDiff); // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
+
+
 
     const PostId = CryptoJS.SHA256(NowDate)['words'][0];
 
